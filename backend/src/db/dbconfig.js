9a80c29ae43ead296921544dd3c92757df64b1fc
Martin Matarrese <matarresemartin@gmail.com>
2025-09-11 15:42:50 -0300

    Corregi un comando de configuración

--- backend/src/db/dbconfig.js
+++ backend/src/db/dbconfig.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const initMongoDB = async() => {
     try {
-        const mongoUrl = process.env.MONGO_URL;
+        const mongoUrl = process.env.MONGO_URL || process.env.MONGO_DOCKER;
         await mongoose.connect(mongoUrl);
     } catch(error) {
         throw new Error(error)
