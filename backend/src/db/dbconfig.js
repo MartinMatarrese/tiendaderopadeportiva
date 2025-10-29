f9589cb844939ab6f54641a54e1fdb4e5caf3884
Martin Matarrese <matarresemartin@gmail.com>
2025-10-29 12:01:28 -0300

    Agregué mas variables de entorno al debug

--- backend/src/db/dbconfig.js
+++ backend/src/db/dbconfig.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose"
 import "dotenv/config";
+
 console.log('Variables de entorno cargadas:', {
   MONGO_URL: process.env.MONGO_URL,
-  NODE_ENV: process.env.NODE_ENV
+  NODE_ENV: process.env.NODE_ENV,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  SECRET_KEY: process.env.SECRET_KEY
 });
+
 export const initMongoDB = async() => {
     try {
         const mongoUrl = process.env.MONGO_URL;
