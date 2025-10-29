f64fb70cc515516c9f2ecf8202973854caf201e1
Martin Matarrese <matarresemartin@gmail.com>
2025-10-29 11:33:13 -0300

    Agregué debug en la conexión

--- backend/src/db/dbconfig.js
+++ backend/src/db/dbconfig.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 import "dotenv/config";
-
+console.log('Variables de entorno cargadas:', {
+  MONGO_URL: process.env.MONGO_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
 export const initMongoDB = async() => {
     try {
         const mongoUrl = process.env.MONGO_URL;
