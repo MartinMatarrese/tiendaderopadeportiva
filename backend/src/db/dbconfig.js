e5b87cff510489fa85c1ebc58a5d723026d96f9e
Martin Matarrese <matarresemartin@gmail.com>
2026-05-20 14:46:40 -0300

    Agregué la nueva url y actualice socket.io

--- backend/src/db/dbconfig.js
+++ backend/src/db/dbconfig.js
@@ -2,10 +2,21 @@ import mongoose from "mongoose"
 import "dotenv/config";
 
 export const initMongoDB = async() => {
+    const local = process.env.MONGO_URL_LOCAL;
+    const url = process.env.MONGO_URL;
+    const env = process.env.NODE_ENV || "development";
+    const isProduction = env === "production";
+    const mongoUrl = isProduction ? url : local
+    
+    if(!mongoUrl) {
+        throw new Error (`MongoDB URL no definida por ${isProduction ? "production" : "development"} enviorenment`);
+    }
+    // const isDevelopment = env === "development";
     try {
-        const mongoUrl = process.env.MONGO_URL;
+        // const mongoUrl = process.env.MONGO_URL;
+        
         await mongoose.connect(mongoUrl);
     } catch(error) {
-        throw new Error(error)
-    }
+        throw error
+    };
 };
